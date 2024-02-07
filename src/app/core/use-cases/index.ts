733eb1b221e09da6d0cae3455101b3856a941989
uFercho <nelsonsuare@gmail.com>
2024-02-07 16:35:00 +0100

    :hammer:fix(index): update index export

--- src/app/core/use-cases/index.ts
+++ src/app/core/use-cases/index.ts
@@ -8,3 +8,9 @@ export * from './pros-cons/pros-cons-stream.use-case'
 export * from './translate/translate.use-case'
 
 export * from './audios/text-to-audio.use-case'
+
+export * from './imaga-generation/image-generation.use-case'
+export * from './imaga-generation/image-variation.use-case'
+
+export * from './assistant/create-thread.use-case'
+export * from './assistant/user-question.use-case'
--- src/app/presentation/components/index.ts
+++ src/app/presentation/components/index.ts
@@ -1,11 +1,12 @@
-export * from "./chat-bubbles/chatMessage/chatMessage.component"
-export * from "./chat-bubbles/myMessage/myMessage.component"
+export * from './chat-bubbles/chatMessage/chatMessage.component'
+export * from './chat-bubbles/myMessage/myMessage.component'
+export * from './chat-bubbles/gptMessageEditableImage/gptMessageEditableImage.component'
 
-export * from "./sidebarMenuItem/sidebarMenuItem.component"
+export * from './sidebarMenuItem/sidebarMenuItem.component'
 
-export * from "./typingLoader/typingLoader.component"
+export * from './typingLoader/typingLoader.component'
 
-export * from "./text-boxes/textMessageBox/textMessageBox.component"
-export * from "./text-boxes/textMessageBoxFile/textMessageBoxFile.component"
-export * from "./text-boxes/textMessageBoxSelect/textMessageBoxSelect.component"
-export * from "./chat-bubbles/gptMessageOrthography/gptMessageOrthography.component"
+export * from './text-boxes/textMessageBox/textMessageBox.component'
+export * from './text-boxes/textMessageBoxFile/textMessageBoxFile.component'
+export * from './text-boxes/textMessageBoxSelect/textMessageBoxSelect.component'
+export * from './chat-bubbles/gptMessageOrthography/gptMessageOrthography.component'
